93a6a0f23d7bf708e7427573d01e56f43947165d
Сергей <sergey.kononov@ema.ru>
2018-10-14 17:29:34 +0700

    Initial Commit

---
+++ Yo.List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yo.List
+//
+//  Created by Сергей on 14/10/2018.
+//  Copyright © 2018 Сергей Кононов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
